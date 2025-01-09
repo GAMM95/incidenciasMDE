290db1da6a31ef0a690a6b86b56944af3e54e08b
Jhonatan Mantilla <116129111+GAMM95@users.noreply.github.com>
2025-01-08 22:45:15 -0500

    depuracion de codigo

---
+++ config/settings.php
@@ -0,0 +1,10 @@
+<?php
+// Nombre de usuario para la conexión cierreController la BD
+define("USUARIO", "sa");
+// Contraseña para la conexión
+define("PASSWORD", "sql");
+// Nombre de la BD
+define("DATABASE", "BD_INCIDENCIAS_2008");
+// Dirección del servidor de la base de datos
+// define("SERVIDOR", "INF08\JHONATANMM");
+define("SERVIDOR", "JHONATANMM\SQL2008R2");
